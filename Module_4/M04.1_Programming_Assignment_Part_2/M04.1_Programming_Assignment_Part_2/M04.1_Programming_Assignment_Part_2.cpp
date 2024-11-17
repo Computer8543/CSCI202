8440171f6cca22406904e437c042624693ac826d
Computer8543 <college1b7e@gmail.com>
2024-11-17 10:02:57 -0500

    I finally finished copying and pasting the US states and capitals into a map for the M04.1 Part 2 Programming Assignment.

---
+++ Module_4/M04.1_Programming_Assignment_Part_2/M04.1_Programming_Assignment_Part_2/M04.1_Programming_Assignment_Part_2.cpp
@@ -0,0 +1,36 @@
+/* Program Name: M04.1_Programming_Assignment_Part_2.cpp
+*  Author: Kyle Ingersoll 
+*  Date last updated 11/17/2024
+*  Purpose: To create a program to store pairs of each state and its capital in a map. The program prompts the user to enter a state and displays the capital for the state.
+*/
+
+#include <iostream>
+#include <string>
+#include <map>
+
+static const std::map<std::string, std::string> stateAndCapitalMap{
+    {"Alabama", "Montgomery"}, {"Alaska", "Juneau"}, {"Arizona", "Phoenix"},
+    {"Arkansas", "Little Rock"}, {"California", "Sacramento"}, {"Colorado", "Denver"},
+    {"Connecticut", "Hartford"}, {"Delaware", "Dover"}, {"Florida", "Tallahassee"},
+    {"Georgia", "Atlanta"}, {"Hawaii", "Honolulu"}, {"Idaho", "Boise"},
+    {"Illinois", "Springfield"}, {"Indiana", "Indianapolis"}, {"Iowa", "Des Moines"}, 
+    {"Kansas", "Topeka"}, {"Kentucky", "Frankfort"}, {"Louisiana", "Baton Rouge"}, 
+    {"Maine", "Augusta"}, {"Maryland", "Annapolis"}, {"Massachusetts", "Boston"},
+    {"Michigan", "Lansing"}, {"Minnesota", "Saint Paul"}, {"Mississippi", "Jackson"},
+    {"Missouri", "Jefferson City"}, {"Montana", "Helena"}, {"Nebraska", "Lincoln"},
+    {"Nevada", "Carson City"}, {"New Hampshire", "Concord"}, {"New Jersey", "Trenton"},
+    {"New Mexico", "Santa Fe"}, {"New York", "Albany"}, {"North Carolina", "Raleigh"},
+    {"North Dakota", "Bismarck"}, {"Ohio", "Columbus"}, {"Oklahoma", "Oklahoma City"},
+    {"Oregon", "Salem"}, {"Pennsylvania", "Harrisburg"}, {"Rhode Island", "Providence"},
+    {"South Carolina", "Columbia"}, {"South Dakota", "Pierre"}, {"Tennessee", "Nashville"},
+    {"Texas", "Austin"}, {"Utah", "Salt Lake City"}, {"Vermont", "Montpelier"},
+    {"Virginia", "Richmond"}, {"Washington", "Olympia"}, {"West Virginia", "Charleston"},
+    {"Wisconsin", "Madison"}, {"Wyoming", "Cheyenne"}
+};
+int main()
+{
+
+    // end program normally
+    return 0;
+}
+
